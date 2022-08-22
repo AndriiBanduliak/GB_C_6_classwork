bfb1aaf87b3497ace502de03811b9679b276862e
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-22 20:56:13 +0300

    1st done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 40: Напишите программу, которая принимает на вход три
+ числа и проверяет, может ли существовать треугольник с сторонами
+ такой длины*/
+using static System.Console;
+using System.Linq;
+Clear();
+
+WriteLine("Enter the sides of the triangle");
+
+int[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>Convert.ToInt32(x)).ToArray();
+
+WriteLine(Triangle(array)?"Yes":"No");
+
+bool Triangle(int[] arra)
+{
+    return (arra[0]+arra[1] > arra[2] && arra[1]+arra[2] > arra[0] && arra[0]+arra[2] > arra[1]);
+    
+}
+
