7d41bd9f4401f7432850cd310fe1d45229fa8c05
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-22 22:12:56 +0300

    5th task done

--- Program.cs
+++ Program.cs
@@ -1,22 +1,8 @@
-﻿/*Задача 44: Не используя рекурсию, выведите первые N 
-чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.*/
-
-using static System.Console;
+﻿using static System.Console;
+using System.Linq;
 Clear();
+WriteLine("Введите массив через пробел:");
 
-WriteLine("Enter the linght of list Fibbonacci  :");
-int number = Convert.ToInt32(ReadLine());
-int[] result = Fibonacci(number);
-WriteLine($"[{String.Join(",", result)}]");
+int count = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray().Where(a=>a%2!=0).Count();
 
-int[] Fibonacci (int n)
-{
-    int[] arr = new int[n];
-    arr[0] = 0;
-    arr[1] = 1;
-    for(int i = 2; i < arr.Length; i++)
-    {
-        arr[i] = arr[i-1] + arr[i-2];
-    }
-    return arr;
-}
+WriteLine(count);
